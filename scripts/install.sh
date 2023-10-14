d06e005de945570eff9254bcd64d52bfca261b54
I am Pierre <i_am_pierre@icloud.com>
2023-10-15 00:05:09 +0800

    Start rework on install.sh, add set-e
    Signed-off-by: I am Pierre <i_am_pierre@icloud.com>

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,38 +2,42 @@
 # File: install.sh
 
 # Test for the platform and install brew or use pkg or apt to install packages
+
 my_os=$(uname -s)
+
+set -x
+
 case $my_os in
     'Linux')
-        echo -e "You are using Linux \n"
+        echo -e "### You are using Linux \n"
 	
 	# Create structure
-	echo -e "Creating working .config structure \n"
+	echo -e "### Creating working .config structure \n"
 	mkdir -p $HOME/.config/{zsh,git,vim}
         ;;
     'FreeBSD')
-        echo -e "You are using FreeBSD \n"
+        echo -e "### You are using FreeBSD \n"
 	
 	# Create structure
-	echo -e "Creating working .config structure \n"
+	echo -e "### Creating working .config structure \n"
 	mkdir -p $HOME/.config/{zsh,git,vim}
 	;;
     'Darwin')
-        echo -e "You Are using macOS \n"
+        echo -e "### You Are using macOS \n"
         
 	# Create structure
-	echo -e "Creating working .config structure \n"
+	echo -e "### Creating working .config structure \n"
 	mkdir -p $HOME/.config/{zsh,git,vim,asdf}
         
         # Source macOS.sh to make it more usable
-        echo -e "Sourcing macOS.sh script \n"
+        echo -e "### Sourcing macOS.sh script \n"
         source ./macOS.sh
         # Install Brew and add it to PATH temporarily
-        echo -e "Installing Homebrew \n"
+        echo -e "### Installing Homebrew \n"
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
         ;;
     *)
-        echo -e "You are using something else, $my_os \n"
+        echo -e "### You are using something else, $my_os \n"
         ;;
 esac
 #################
