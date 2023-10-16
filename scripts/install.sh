1c86a05f757ab51d44b42cabbac080f8de2af3f4
I am Pierre <i_am_pierre@icloud.com>
2023-10-16 18:22:09 +0800

    Refector Install.sh and minor update to macOS.sh
    Signed-off-by: I am Pierre <i_am_pierre@icloud.com>

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # File: install.sh
 
 my_pkg_list=( 
@@ -12,75 +12,73 @@ my_pkg_list=(
 
 my_os=$(uname -s)
 
+# Install function
+install_packages() {
+    local os=$1
+    local pkg_list=("${!2}")
+
+    case $os in
+        'Linux')
+            if [ -x "$(command -v apt)" ]; then
+                # Debian-based Linux (e.g., Ubuntu)
+                echo "### You are using Debian-based Linux"
+                sudo apt update && sudo apt install -y "${pkg_list[@]}"
+            else
+                echo "### Unsupported Linux distribution"
+                exit 1
+            fi
+            ;;
+
+        'FreeBSD')
+            if [ -x "$(command -v pkg)" ]; then
+                echo "### You are using FreeBSD"
+                sudo pkg update && sudo pkg install -y "${pkg_list[@]}"
+            else
+                echo "### Unsupported OS (FreeBSD with pkg not found)"
+                exit 1
+            fi
+            ;;
+
+        'Darwin')
+            echo "### You are using macOS"
+            ;;
+
+        *)
+            echo "### You are using an unsupported OS, $my_os"
+            exit 1
+            ;;
+    esac
+}
+
 # Test for the platform and install packages accordingly
-case $my_os in
-    'Linux')
-        if [ -x "$(command -v apt)" ]; then
-            # Debian-based Linux (e.g., Ubuntu)
-            echo -e "### You are using Debian-based Linux \n"
-            sudo apt update && sudo apt install -y "${my_pkg_list[@]}"
-        else
-            echo -e "### Unsupported Linux distribution \n"
-            exit 1
-        fi
+install_packages "$my_os" my_pkg_list
 
-    	# Create structure for .config files
-	    echo -e "### Creating working .config structure \n"
-	    mkdir -p "$HOME"/.config/{zsh,git,vim}
+# Create structure for .config files
+echo "### Creating working .config structure"
+mkdir -p "$HOME/.config/{zsh,git,vim}"
 
-        # Stow .dotfiles
-        echo -e "### Creating simlink using Stow"
-        cd ~/.dotfiles || exit 1
-        stow -vSt ~ git tmux vim zsh
+# Stow .dotfiles
+echo "### Creating symlink using Stow"
+cd ~/.dotfiles || exit 1
+stow -vSt ~ git tmux vim zsh
 
-        # Updating shell to zsh for user
-        echo -e "### Updating shell to zsh"
-        chsh -s /bin/zsh "$USER"
-        ;;
+# Updating shell to zsh for the user
+echo "### Updating shell to zsh"
+chsh -s "$(command -v zsh)" "$USER"
 
-      'FreeBSD')
-        if [ -x "$(command -v pkg)" ]; then
-            echo -e "### You are using FreeBSD \n"
-            sudo pkg update && sudo pkg install -y "${my_pkg_list[@]}"
-        else
-            echo -e "### Unsupported OS (FreeBSD with pkg not found) \n"
-            exit 1
-        fi
-       
-    	# Create structure for .config files
-	    echo -e "### Creating working .config structure \n"
-	    mkdir -p "$HOME"/.config/{zsh,git,vim}
+if [ "$my_os" == 'Darwin' ]; then
+    # Additional setup for macOS
+    echo "### Creating working .config structure for asdf"
+    mkdir -p "$HOME/.config/asdf"
+    
+    # Source macOS.sh to make it more usable
+    echo "### Sourcing macOS.sh script"
+    source ./macOS.sh
+    
+    # Install Homebrew
+    echo "### Installing Homebrew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
-        # Stow .dotfiles
-        echo -e "### Creating simlink using Stow"
-        cd ~/.dotfiles || exit 1
-        stow -vSt ~ git tmux vim zsh
-
-        # Updating shell to zsh for user
-        echo -e "### Updating shell to zsh"
-        chsh -s /usr/local/bin/zsh "$USER"
-	;;
-
-      'Darwin')
-        echo -e "### You are using macOS \n"
-        
-        # Create structure
-        echo -e "### Creating working .config structure \n"
-        mkdir -p "$HOME"/.config/{zsh,git,vim,asdf}
-        
-        # Source macOS.sh to make it more usable
-        echo -e "### Sourcing macOS.sh script \n"
-        source ./macOS.sh
-        
-        # Install Brew and add it to PATH temporarily
-        echo -e "### Installing Homebrew \n"
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-        ;;
-        
-    *)
-        echo -e "### You are using an unsupported OS, $my_os \n"
-        ;;
-esac
-
-echo -e "### Install completed"
-echo -e "### Consider exec zsh -l to load zsh now\n or logout"
+echo "### Installation completed"
+echo "### Consider running 'exec zsh -l' to load zsh now, or log out"
