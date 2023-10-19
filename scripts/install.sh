685caa07335b005938cc180b1a932400fd230881
I am Pierre <i_am_pierre@icloud.com>
2023-10-19 20:30:10 +0800

    Fix variable name

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # File: install.sh
 
-my_pkg_list=( 
+pkg_list=( 
     bat
     git
     pwgen 
@@ -11,52 +11,72 @@ my_pkg_list=(
     zsh 
 )
 
-my_os=$(uname -s)  # Corrected variable name
+my_os=$(uname -s)
 
-case $my_os in  # Corrected variable name
-    'Linux')
-        if [ -x "$(command -v apt)" ]; then
-            # Debian-based Linux (e.g., Ubuntu)
-            echo "### You are using Debian-based Linux"
-            sudo apt update && sudo apt install -y "${my_pkg_list[@]}" || { echo "Failed to install packages"; exit 1; }
-        else
-            echo "### Unsupported Linux distribution"
+case $my_os in
+        'Linux')
+            if [ -x "$(command -v apt)" ]; then
+                # Debian-based Linux (e.g., Ubuntu)
+                echo "### You are using Debian-based Linux"
+                sudo apt update && sudo apt install -y "${pkg_list[@]}" || { echo "Failed to install packages"; exit 1; }
+            else
+                echo "### Unsupported Linux distribution"
+                exit 1
+            fi
+            ;;
+
+        'FreeBSD')
+            if [ -x "$(command -v pkg)" ]; then
+                echo "### You are using FreeBSD"
+                sudo pkg update && sudo pkg install -y "${pkg_list[@]}" || { echo "Failed to install packages"; exit 1; }
+            else
+                echo "### Unsupported OS (FreeBSD with pkg not found)"
+                exit 1
+            fi
+            ;;
+
+        'Darwin')
+            echo "### You are using macOS"
+
+            if ! [ -x "$(command -v brew)" ]; then
+                # Install Homebrew if it's not already installed
+                echo "### Installing Homebrew"
+                /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" || { echo "Failed to install Homebrew"; exit 1; }
+            fi
+
+            # Additional setup for macOS (asdf)
+            echo "### Creating working .config structure for asdf"
+            mkdir -p "$HOME/.config/asdf"
+    
+            # Source macOS.sh to make it more usable
+            echo "### Sourcing macOS.sh script"
+            source ./macOS.sh
+            ;;
+
+        *)
+            echo "### You are using an unsupported OS, $my_os"
             exit 1
-        fi
-        ;;
-
-    'FreeBSD')
-        if [ -x "$(command -v pkg)" ]; then
-            echo "### You are using FreeBSD"
-            sudo pkg update && sudo pkg install -y "${my_pkg_list[@]}" || { echo "Failed to install packages"; exit 1; }
-        else
-            echo "### Unsupported OS (FreeBSD with pkg not found)"
-            exit 1
-        fi
-        ;;
-
-    'Darwin')
-        echo "### You are using macOS"
-
-        if ! [ -x "$(command -v brew)" ]; then
-            # Install Homebrew if it's not already installed
-            echo "### Installing Homebrew"
-            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" || { echo "Failed to install Homebrew"; exit 1; }
-        fi
-
-        # Additional setup for macOS (asdf)
-        echo "### Creating working .config structure for asdf"
-        mkdir -p "$HOME/.config/asdf"
-
-        # Source macOS.sh to make it more usable
-        echo "### Sourcing macOS.sh script"
-        source ./macOS.sh
-        ;;
-
-    *)
-        echo "### You are using an unsupported OS, $my_os"
-        exit 1
-        ;;
+            ;;
 esac
 
-# The rest of the script remains unchanged
+# Check if HOME is set
+if [ -z "$HOME" ]; then
+    echo "The HOME environment variable is not set. Please set it before running this script."
+    exit 1
+fi
+
+# Create structure for .config files
+echo "### Creating working .config structure"
+mkdir -p "$HOME/.config/{zsh,git,vim}"
+
+# Stow .dotfiles
+echo "### Creating symlink using Stow"
+cd "$HOME/.dotfiles" || { echo "Failed to change directory"; exit 1; }
+stow -vSt "$HOME" git tmux vim zsh || { echo "Failed to stow dotfiles"; exit 1; }
+
+# Updating shell to zsh for the user
+echo "### Updating shell to zsh"
+chsh -s "$(command -v zsh)" "$USER" || { echo "Failed to update shell to zsh"; exit 1; }
+
+echo "### Installation completed"
+echo "### Consider running 'exec zsh -l' to load zsh now, or log out"
