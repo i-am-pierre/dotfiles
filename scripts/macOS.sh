d139f9ab520b8c93a99d1768cda9e486e971179f
I am Pierre <i_am_pierre@icloud.com>
2023-11-20 00:12:57 +0800

    Fix Typo in macOS.sh

--- scripts/macOS.sh
+++ scripts/macOS.sh
@@ -86,7 +86,7 @@ defaults write com.apple.Safari WarnAboutFraudulentWebsites -bool true
 echo "Terminal..."
 # Copy my prefences plist into ~/Library/Prefrences
 if [ -f "com.apple.Terminal.plist" ] ; then
-  cp com.apple.Terminal.plist $HOME/Library/Preferences || { echo "Failed to copy Termincal.plist"; exit 1; }
+  cp com.apple.Terminal.plist "$HOME"/Library/Preferences || { echo "Failed to copy Terminal.plist"; exit 1; }
 fi
 
 for app in "Activity Monitor" \
